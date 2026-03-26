3fbe8e2178e5e5c68f4aa6785bb304336dbc1788
MakanakaMawunga <159343898+MakanakaMawunga@users.noreply.github.com>
2026-03-26 16:57:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
-        recommended_routes=accessible_routes[:3]
-    )
+import json
 
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
 @app.route("/saved-routes")
 def saved_routes():
